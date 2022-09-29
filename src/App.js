ad7522f0acb713222032309795e3420e6729bfaf
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2022-09-29 22:36:20 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavRoutes from "./routes/NavRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AuthContextProvider> */}
+        <NavRoutes />
+      {/* </AuthContextProvider> */}
+    </div>
+  );
+}
+
+export default App;
